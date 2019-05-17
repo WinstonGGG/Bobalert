8ff48d3a38875a000fb2f26ec07d9474dc95f7fd
Liuwenda (Winston) Gao <lgao9@ucsc.edu>
2019-05-16 17:31:36 -0700

    some sample code

---
+++ hello-world/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Bobalert</h1>
+    <table>
+      <tr>
+        <th>Boba</th>
+        <th>Today</th>
+        <th>upcomming</th>
+        <th>Filter</th>
+      </tr>
+    </table>
+    <Fundraiser/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Fundraiser from './components/Fundraiser.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld, 
+    Fundraiser
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/Fundraiser.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="fund">
+    <h2>Fundraiser Name</h2>
+    <h3>Organization Name</h3>
+    <h3>Location; Time</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fundrasier',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
