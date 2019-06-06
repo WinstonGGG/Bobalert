0b5a1f47e18c3611de16230ec9441d46ee56f3d6
Dan Vo <datvo@ucsc.edu>
2019-06-05 17:37:04 -0700

    fixed stuff

--- hello-world/src/components/Filter.vue
+++ hello-world/src/components/Filter.vue
@@ -7,8 +7,8 @@
     </div>
     <div v-if="showDropDown" class="dropdown-content">
       <h1> Locations: </h1>
-      <input type="checkbox" id="location" v-model="quarry"> Quarry<br>
-      <input type="checkbox" id="location" v-model="sne"> SnE<br>
+      <input type="checkbox" id="location1"/><label for ="location1">Quarry</label><br>
+      <input type="checkbox" id="location2"/><label for ="location2">SnE</label><br>
       <div class="buttonStyle">
         <input type="button" onclick="submitFunction()" id="Apply" value="Apply">
       </div>
@@ -35,7 +35,7 @@ export default {
 /* Dropdown content (hidden by default) */
 .dropdown-content {
   position: absolute;
-  width: 24.76%;
+  width: 22.25%;
   background-color: #003c6c;
   list-style-type: none;
 }
@@ -45,6 +45,10 @@ export default {
   float: none;
   text-decoration: inherit;
   /* display: block; */
+}
+
+label {
+  color: white;
 }
 
 input[type=checkbox] {
